4023f34efa227092d7d8cab3566132990df36637
angelmarte <robotking134@gmail.com>
2023-03-05 16:30:44 -0500

    flask starting out

--- backend/flaskr/app.py
+++ backend/flaskr/app.py
@@ -1,6 +1,20 @@
+from flask_login import LoginManager
 from flask import Flask
+
 app = Flask(__name__)
+login_manager = LoginManager()
 
 @app.route("/")
 def home():
     return "Hello, Flask!"
+
+@app.route("/members")
+def members():
+    return {"members": ["member 1","member 2", "member 3"]}
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
