69d27f8f5737aa0775976e4878b0f3230b4ed877
Maxim <polyakovmaxim05@gmail.com>
2023-03-07 12:07:34 -0500

    init

--- backend/flaskr/models.py
+++ backend/flaskr/models.py
@@ -1,8 +1,23 @@
 from . import db
 
-# user model
-class Users():
-    id = db.Column(db.integer, primary_key=True)
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(120), nullable=False)
     password_hash = db.Column(db.String(128))
+    spreadsheets = db.relationship("Spreadsheet", backref="user", lazy=True)
+
+class Spreadsheet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    string_value = db.Column(db.String(225), nullable=True)
+    integer_value = db.Column(db.Integer, nullable=True)
+    columns= db.relationship("Column", backref="spreadsheet", lazy=True)
+
+class Column(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(225), nullable=True)
+    numbers = db.Column(db.Integer, nullable=True)
+    total = db.Column(db.Integer, nullable=True)
+    
+
